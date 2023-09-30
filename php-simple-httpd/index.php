95f5798e1bd86b402faa66e43d983d6b6ae42e8a
machih <machih@vmware.com>
2023-09-30 23:08:41 +0900

    First Commit

---
+++ php-simple-httpd/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Simple</title>
+</head>
+<body>
+<?php
+echo '<p>Hello World!</p>';
+
+$names = $_SERVER['QUERY_STRING'];
+foreach (explode(",", $names) as $name) {
+    if (extension_loaded($name)) {
+        echo 'SUCCESS: ' . $name . ' loads.';
+    }
+    else {
+        echo 'ERROR: ' . $name . ' failed to load.';
+    }
+}
+?>
+</body>
+</html>
---
+++ php-simple-w-custom-bindings/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP w Custom Bindings</title>
+</head>
+<body>
+<?php
+$db_host="";
+$db_port="";
+$db_username="";
+$db_password="";
+$db_database="";
+if(getenv('SERVICE_BINDING_ROOT') != ""){
+    $sbRoot = getenv('SERVICE_BINDING_ROOT');
+    $sbDirs = scandir($sbRoot);
+    foreach ($sbDirs as $sbDir) {
+        $sbName = $sbRoot . '/' . $sbDir;
+        if (is_dir($sbName)) {
+            $sbContents = scandir($sbName);
+            foreach ($sbContents as $content) {
+                if ($content == "type" &&
+                    is_file($sbName . '/' . $content) &&
+                    file_get_contents($sbName . '/' . $content) == "postgresql"
+                ){
+                    $db_host = file_get_contents($sbName . '/host');
+                    $db_port = file_get_contents($sbName . '/port');
+                    $db_username = file_get_contents($sbName . '/username');
+                    $db_password = file_get_contents($sbName . '/password');
+                    $db_database = file_get_contents($sbName . '/password');
+                }
+
+            }
+
+        }
+    }
+}
+$connectionString = "host=$db_host dbname=$db_database user=$db_username password=$db_password port=$db_port";
+echo $connectionString . "<br>";
+$con = pg_connect($connectionString);
+$sql = "SELECT version();";
+$result = pg_send_query($con, $sql);
+echo $result . "<br>";
+pg_close($con)
+?>
+</body>
+</html>
